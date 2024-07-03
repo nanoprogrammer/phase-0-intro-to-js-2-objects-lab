4e340b33011bab4ee643fcdd480c45e7833ee02d
Samson Njai <njaisamson@gmail.com>
2024-07-03 13:01:59 +0300

    adds the required functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution in this file!
+const employee = {
+    name : "Mary",
+    streetAddress : "Nairobi",
+}
+
+function updateEmployeeWithKeyAndValue(employeeList, key, value) {
+    const newEmployee = {...employeeList};
+
+    newEmployee[key] = value;
+    return newEmployee;
+    
+};
+updateEmployeeWithKeyAndValue(
+    employee,
+    "John",
+    "Thika"
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeList, key, value) {
+    employeeList[key] = value;
+    return employeeList;
+    
+};
+destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+    "Ann",
+    "Mombasa",
+);
+
+function deleteFromEmployeeByKey(employeeList) {
+    const newEmployee = {...employeeList};
+
+    delete newEmployee.name;
+    return newEmployee;
+};
+deleteFromEmployeeByKey(employee);
+
+function destructivelyDeleteFromEmployeeByKey(employeeList) {
+    delete employeeList.name;
+    return employeeList;
+}
+destructivelyDeleteFromEmployeeByKey(employee);
